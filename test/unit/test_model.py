0f545aef2e14ca426f203ff45bf79ae5ea2f66af
FredLoney <loneyf@ohsu.edu>
2015-05-26 18:33:37 -0700

    Test necrosis_percent_as_score.

--- test/unit/test_model.py
+++ test/unit/test_model.py
@@ -5,9 +5,14 @@ from mongoengine.connection import get_db
 from nose.tools import (assert_true, assert_false, assert_equal, assert_raises)
 from qiprofile_rest_client.model.subject import Subject
 from qiprofile_rest_client.model.imaging import (
-  Session, Scan, ScanProtocol, Registration, RegistrationProtocol, LabelMap,
-  VoxelSize, SessionDetail, Volume, Point, Region, Modeling, ModelingProtocol)
-from qiprofile_rest_client.model.clinical import (Encounter, Evaluation, TNM)
+    Session, Scan, ScanProtocol, Registration, RegistrationProtocol, LabelMap,
+    VoxelSize, SessionDetail, Volume, Point, Region, Modeling, ModelingProtocol
+)
+from qiprofile_rest_client.model.clinical import (
+    Encounter, Evaluation, TNM, FNCLCCGrade, NecrosisPercentValue,
+    NecrosisPercentRange, necrosis_percent_as_score
+)
+
 
 class TestModel(object):
     """
@@ -30,32 +35,32 @@ class TestModel(object):
         
         subject.collection='Breast'
         subject.save()
-
+    
     def test_race(self):
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         subject.races = ['White', 'Black', 'Asian', 'AIAN', 'NHOPI']
         subject.save()
-
+    
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         subject.races = ['Invalid']
         with assert_raises(ValidationError):
             subject.save()
-
+    
         # Races must be a list.
         subject.races = 'White'
         with assert_raises(ValidationError):
             subject.save()
-
+    
     def test_ethnicity(self):
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         subject.ethnicity = 'Non-Hispanic'
         subject.save()
-
+    
         # The ethnicity is a controlled value.
         subject.ethnicity = 'Invalid'
         with assert_raises(ValidationError):
             subject.save()
-
+    
     def test_encounter(self):
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         date = datetime(2013, 1, 4)
@@ -63,7 +68,7 @@ class TestModel(object):
         subject.encounters = [encounter]
         # The encounter outcome is optional.
         subject.save()
-
+    
         # Add the evaluation.
         evaluation = Evaluation()
         outcome = TNM()
@@ -71,7 +76,7 @@ class TestModel(object):
         encounter.evaluation = evaluation
         # Save the subject and embedded encounter.
         subject.save()
-
+    
     def test_tnm_size(self):
         for value in ['T1', 'Tx', 'cT4', 'T1b', 'cT2a']:
             size = TNM.Size.parse(value)
@@ -79,17 +84,42 @@ class TestModel(object):
                                            " expected %s, found %s"
                                            % (value, str(size)))
 
+    def test_necrosis_score(self):
+        fixture = {
+            0: dict(integer=0,
+                    value=NecrosisPercentValue(value=0),
+                    range=NecrosisPercentRange(
+                        start=NecrosisPercentRange.LowerBound(value=0),
+                        stop=NecrosisPercentRange.UpperBound(value=1))),
+            1: dict(integer=40,
+                    value=NecrosisPercentValue(value=40),
+                    range=NecrosisPercentRange(
+                        start=NecrosisPercentRange.LowerBound(value=40),
+                        stop=NecrosisPercentRange.UpperBound(value=50))),
+            2: dict(integer=50,
+                    value=NecrosisPercentValue(value=50),
+                    range=NecrosisPercentRange(
+                        start=NecrosisPercentRange.LowerBound(value=50),
+                        stop=NecrosisPercentRange.UpperBound(value=60)))
+        }
+        for expected, inputs in fixture.iteritems():
+            for in_type, in_val in inputs.iteritems():
+                actual = necrosis_percent_as_score(in_val)
+                assert_equal(actual, expected, "The necrosis score for %s"
+                                               "is incorrect: %d" %
+                                               (actual, expected))
+    
     def test_session(self):
         # The session parent.
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         # The session.
         date = datetime(2013, 1, 4)
         session = Session(acquisition_date=date)
-
+    
         # Save the subject and embedded session.
         subject.sessions = [session]
         subject.save()
-
+    
     def test_modeling(self):
         # The session subject.
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
@@ -109,13 +139,13 @@ class TestModel(object):
         result = dict(ktrans=ktrans)
         modeling = Modeling(protocol=protocol, source=source, resource='pk_01',
                        result=result)
-
+    
         # Save the subject and embedded session modeling.
         date = datetime(2014, 3, 1)
         session = Session(acquisition_date=date, modelings=[modeling])
         subject.sessions = [session]
         subject.save()
-
+    
     def test_scan(self):
         # The scan protocol.
         voxel_size = VoxelSize(width=2, depth=4, spacing=2.4)
@@ -125,11 +155,11 @@ class TestModel(object):
                                                          defaults=pcl_defs)
         # The scan.
         scan = Scan(protocol=protocol, number=1)
-
+    
         # Save the session detail and embedded scan.
         detail = SessionDetail(scans=[scan])
         detail.save()
-
+    
     def test_bolus_arrival(self):
         # The scan protocol.
         protocol, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
@@ -146,18 +176,18 @@ class TestModel(object):
         scan.volumes = [create_volume(i + 1) for i in range(32)]
         # The bolus arrival is now valid.
         detail.save()
-
+    
         # The bolus arrival index must refer to a volume.
         scan.bolus_arrival_index = 32
         with assert_raises(ValidationError):
             detail.save()
-
+    
     def test_registration(self):
         # The scan protocol.
         scan_pcl, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
         # The scan.
         scan = Scan(protocol=scan_pcl, number=1)
-
+    
         # The registration protocol.
         reg_params = dict(transforms=['Rigid', 'Affine', 'SyN'])
         reg_pcl_defs = dict(parameters=reg_params)
@@ -172,13 +202,13 @@ class TestModel(object):
         detail = SessionDetail(scans=[scan])
         detail.scans = [scan]
         detail.save()
-
+    
     def test_roi(self):
         # The scan protocol.
         protocol, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
         # The scan.
         scan = Scan(protocol=protocol, number=1)
-
+    
         # The ROI.
         mask = '/path/to/mask.nii.gz'
         label_map = LabelMap(filename='/path/to/label_map.nii.gz',
@@ -187,7 +217,7 @@ class TestModel(object):
         intensity = 31
         roi = Region(mask=mask, label_map=label_map, centroid=centroid,
                      average_intensity=intensity)
-
+    
         # Save the session detail and embedded scan ROI.
         scan.rois = [roi]
         detail = SessionDetail(scans=[scan])
