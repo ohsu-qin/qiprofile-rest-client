bb456a9735b132f1f024827a77807ff8396654bd
FredLoney <loneyf@ohsu.edu>
2015-06-03 10:38:15 -0700

    hormone_receptors is a list.

--- test/unit/test_model.py
+++ test/unit/test_model.py
@@ -9,8 +9,9 @@ from qiprofile_rest_client.model.imaging import (
     VoxelSize, SessionDetail, Volume, Point, Region, Modeling, ModelingProtocol
 )
 from qiprofile_rest_client.model.clinical import (
-    Encounter, Evaluation, TNM, FNCLCCGrade, NecrosisPercentValue,
-    NecrosisPercentRange, necrosis_percent_as_score
+    Biopsy, Evaluation, BreastPathology, TNM, ModifiedBloomRichardsonGrade,
+    HormoneReceptorStatus, FNCLCCGrade, NecrosisPercentValue, NecrosisPercentRange,
+    necrosis_percent_as_score
 )
 
 
@@ -23,44 +24,44 @@ class TestModel(object):
         connect(db='qiprofile_test')
         self.db = get_db()
         self.db.connection.drop_database('qiprofile_test')
-    
+
     def tearDown(self):
-      self.db.connection.drop_database('qiprofile_test')
+        self.db.connection.drop_database('qiprofile_test')
 
     def test_subject(self):
         subject = Subject(project='QIN_Test', number=1)
         # The subject must have a collection.
         with assert_raises(ValidationError):
             subject.save()
-        
+
         subject.collection='Breast'
         subject.save()
-    
+
     def test_race(self):
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         subject.races = ['White', 'Black', 'Asian', 'AIAN', 'NHOPI']
         subject.save()
-    
+
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         subject.races = ['Invalid']
         with assert_raises(ValidationError):
             subject.save()
-    
+
         # Races must be a list.
         subject.races = 'White'
         with assert_raises(ValidationError):
             subject.save()
-    
+
     def test_ethnicity(self):
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         subject.ethnicity = 'Non-Hispanic'
         subject.save()
-    
+
         # The ethnicity is a controlled value.
         subject.ethnicity = 'Invalid'
         with assert_raises(ValidationError):
             subject.save()
-    
+
     def test_biopsy(self):
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         # The pathology.
@@ -71,10 +72,9 @@ class TestModel(object):
         tnm = TNM(tumor_type='Breast', grade=grade, size=size,
                   metastasis=False, resection_boundaries=1,
                   lymphatic_vessel_invasion=False)
-        hormone_receptors = BreastReceptorStatus(
-            estrogen=HormoneReceptorStatus(hormone='estrogen', positive=True,
-                                           intensity=80)
-        )
+        estrogen=HormoneReceptorStatus(hormone='estrogen', positive=True,
+                                       intensity=80)
+        hormone_receptors = [estrogen]
         pathology = BreastPathology(tnm=tnm,
                                     hormone_receptors=hormone_receptors)
         # Add the encounter to the subject.
@@ -83,7 +83,8 @@ class TestModel(object):
         subject.encounters = [biopsy]
         # Save the subject and embedded biopsy.
         subject.save()
-    
+
+
     def test_tnm_size(self):
         for value in ['T1', 'Tx', 'cT4', 'T1b', 'cT2a']:
             size = TNM.Size.parse(value)
@@ -115,44 +116,44 @@ class TestModel(object):
                 assert_equal(actual, expected, "The necrosis score for %s"
                                                "is incorrect: %d" %
                                                (actual, expected))
-    
+
     def test_session(self):
         # The session parent.
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
         # The session.
         date = datetime(2013, 1, 4)
         session = Session(acquisition_date=date)
-    
+
         # Save the subject and embedded session.
         subject.sessions = [session]
         subject.save()
-    
+
     def test_modeling(self):
         # The session subject.
         subject = Subject(project='QIN_Test', collection='Breast', number=1)
-        
+
         # The modeling protocol.
         pcl_defs = dict(input_parameters=dict(r10_val=0.7))
         protocol, _ = ModelingProtocol.objects.get_or_create(
             technique='Bolero', defaults=pcl_defs
         )
-        
+
         # The source protocol.
         scan_pcl, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
         source = Modeling.Source(scan=scan_pcl)
-        
+
         # The modeling data.
         ktrans = Modeling.ParameterResult(filename='/path/to/ktrans.nii.gz')
         result = dict(ktrans=ktrans)
         modeling = Modeling(protocol=protocol, source=source, resource='pk_01',
                        result=result)
-    
+
         # Save the subject and embedded session modeling.
         date = datetime(2014, 3, 1)
         session = Session(acquisition_date=date, modelings=[modeling])
         subject.sessions = [session]
         subject.save()
-    
+
     def test_scan(self):
         # The scan protocol.
         voxel_size = VoxelSize(width=2, depth=4, spacing=2.4)
@@ -162,11 +163,11 @@ class TestModel(object):
                                                          defaults=pcl_defs)
         # The scan.
         scan = Scan(protocol=protocol, number=1)
-    
+
         # Save the session detail and embedded scan.
         detail = SessionDetail(scans=[scan])
         detail.save()
-    
+
     def test_bolus_arrival(self):
         # The scan protocol.
         protocol, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
@@ -177,24 +178,24 @@ class TestModel(object):
         # The bolus arrival index must refer to an existing volume.
         with assert_raises(ValidationError):
             detail.save()
-        
+
         # The scan volumes.
         create_volume = lambda number: Volume(filename="volume%03d.nii.gz" % number)
         scan.volumes = [create_volume(i + 1) for i in range(32)]
         # The bolus arrival is now valid.
         detail.save()
-    
+
         # The bolus arrival index must refer to a volume.
         scan.bolus_arrival_index = 32
         with assert_raises(ValidationError):
             detail.save()
-    
+
     def test_registration(self):
         # The scan protocol.
         scan_pcl, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
         # The scan.
         scan = Scan(protocol=scan_pcl, number=1)
-    
+
         # The registration protocol.
         reg_params = dict(transforms=['Rigid', 'Affine', 'SyN'])
         reg_pcl_defs = dict(parameters=reg_params)
@@ -203,19 +204,19 @@ class TestModel(object):
         )
         # The registration.
         reg = Registration(protocol=reg_pcl, resource='reg_h3Fk5')
-        
+
         # Save the session detail and embedded scan registration.
         scan.registrations = [reg]
         detail = SessionDetail(scans=[scan])
         detail.scans = [scan]
         detail.save()
-    
+
     def test_roi(self):
         # The scan protocol.
         protocol, _ = ScanProtocol.objects.get_or_create(scan_type='T1')
         # The scan.
         scan = Scan(protocol=protocol, number=1)
-    
+
         # The ROI.
         mask = '/path/to/mask.nii.gz'
         label_map = LabelMap(filename='/path/to/label_map.nii.gz',
@@ -224,7 +225,7 @@ class TestModel(object):
         intensity = 31
         roi = Region(mask=mask, label_map=label_map, centroid=centroid,
                      average_intensity=intensity)
-    
+
         # Save the session detail and embedded scan ROI.
         scan.rois = [roi]
         detail = SessionDetail(scans=[scan])
